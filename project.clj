e05fa6d6b5987041f57cfcabd9eb39c952b393cb
Alan Busby <thebusby@gmail.com>
2013-08-18 16:20:45 +0900

    Updated project.clj to use new leiningen format and lwjgl 2.9.0

--- project.clj
+++ project.clj
@@ -1,28 +1,11 @@
-;; NOTE! This project requires leiningen 2.1.0 or later.
-(require 'leiningen.core.eval)
-;;(println (leiningen.core.eval/get-os)) ;; try lein deps to see this
-
-(def LWJGL-CLASSIFIER
-  "Per os native code classifier"
-  {:macosx "natives-osx"
-   :linux "natives-linux"
-   :windows "natives-windows"}) ;; TESTME
-
-(defn lwjgl-classifier
-  "Return the os-dependent lwjgl native-code classifier"
-  []
-  (let [os (leiningen.core.eval/get-os)]
-    (get LWJGL-CLASSIFIER os)))
-
 (defproject hello_lwjgl "0.1.0-SNAPSHOT"
   :description "Simple LWJGL clojure test."
-  :url "https://github.com/rogerallen/hello_lwjgl"
+  :url "https://github.com/thebusby/hello_lwjgl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.lwjgl.lwjgl/lwjgl "2.8.5"]
-                 [org.lwjgl.lwjgl/lwjgl_util "2.8.5"]
-                 [org.lwjgl.lwjgl/lwjgl-platform "2.8.5" 
-                  :classifier ~(lwjgl-classifier)
-                  :native-prefix ""]]
-  :main hello-lwjgl.core)
+  :dependencies [[org.clojure/clojure "1.5.1"] 
+                 [org.lwjgl.lwjgl/lwjgl "2.9.0"] 
+                 [org.lwjgl.lwjgl/lwjgl_util "2.9.0"] 
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0" :classifier "natives-osx" :native-prefix ""]]
+  :main hello-lwjgl.core
+  :aot :all)
